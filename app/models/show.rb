3fb4b0388f30020dc2fa4eaeb4ff4aea8583f73f
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-18 02:15:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,12 @@
 class Show < ActiveRecord::Base
     
-    def self.highest_rating
-      maximum(:rating)  
-    end
+  def self.highest_rating
+    maximum(:rating)  
+  end
+  
+  def self.most_popular_show
+    highest_rating(:name)
+  end
   
   
   
